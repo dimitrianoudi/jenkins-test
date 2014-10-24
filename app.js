94841aac89bbcbbd5fad6a672c6ecb345f466a6b
Dimitri Anoudi <dimitrianoudi@mute.home>
2014-10-24 13:56:25 +0100

    Initial Node app

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+    app = express();
+ 
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+ 
+app.listen(process.env.PORT || 5000);
+ 
+module.exports = app;
